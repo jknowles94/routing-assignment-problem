daf6f8010e4ef727be2e712ae59bf349c916dfe2
Jake Knowles <jakeyknowles@hotmail.co.uk>
2019-02-13 17:29:20 +0000

    Finished task

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Course extends Component {
     render () {
+        console.log("here");
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{this.props.location.state.title}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+import Course from '../Course/Course';
 
 import './Courses.css';
 
@@ -18,10 +20,19 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
+                            return (
+                                <Link 
+                                to={
+                                    {
+                                        pathname: this.props.match.path + '/' + course.id,
+                                        state: {title: course.title}
+                                    }
+                                } key={course.id}><article className="Course">{course.title}</article></Link>
+                            );
                         } )
                     }
                 </section>
+                <Route path={this.props.match.path + '/:id'} component={Course}/>
             </div>
         );
     }
